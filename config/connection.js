7240aeeefc9d6df22cb98e7ad2f184ea19ded6f6
kwu3 <kwu3@villanova.edu>
2020-06-08 15:29:53 -0400

    Fixed connection var

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
